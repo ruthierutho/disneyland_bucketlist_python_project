8a83b3332ea083aab1a195121d1353103f0c78ed
Ruth Foulis <ruthlilfoulis@gmail.com>
2020-11-10 16:26:34 +0000

    deleted visit_count from attraction model

--- models/attraction.py
+++ models/attraction.py
@@ -1,9 +1,8 @@
 class Attraction:
-    def __init__(self, name, land, visited, visit_count, notes, id = None):
+    def __init__(self, name, land, visited, notes, id = None):
         self.name = name
         self.land = land
         self.visited = visited
-        self.visit_count = visit_count
         self.notes = notes
         self.id = id
 
