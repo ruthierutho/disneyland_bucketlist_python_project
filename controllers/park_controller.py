6af876c8d2288752a99c4ec5f534b6d14b2cd79b
Ruth Foulis <ruthlilfoulis@gmail.com>
2020-11-10 10:56:22 +0000

    can add new park

--- controllers/park_controller.py
+++ controllers/park_controller.py
@@ -11,3 +11,18 @@ parks_blueprint = Blueprint("parks", __name__)
 def parks():
     parks = park_repository.select_all()
     return render_template("parks/index.html", all_parks = parks)
+
+#NEW
+@parks_blueprint.route("/parks/new", methods=['GET'])
+def new_park():
+    parks = park_repository.select_all()
+    return render_template("parks/new.html", parks = parks)
+
+#CREATE
+@parks_blueprint.route("/parks", methods=['POST'])
+def create_park():
+    name = request.form['name']
+    park = Park(name)
+    park_repository.save(park)
+    return redirect('/lands')
+
