8bc53c535725b5966dba80e3d2e6310a57ae58ee
Ruth Foulis <ruthlilfoulis@gmail.com>
2020-11-10 11:17:17 +0000

    can add new land

--- controllers/land_controller.py
+++ controllers/land_controller.py
@@ -11,3 +11,21 @@ lands_blueprint = Blueprint("lands", __name__)
 def lands():
     lands = land_repository.select_all()
     return render_template("lands/index.html", lands = lands)
+
+#NEW
+@lands_blueprint.route("/lands/new", methods=['GET'])
+def new_land():
+    lands = land_repository.select_all()
+    parks = park_repository.select_all()
+    return render_template("lands/new.html", lands = lands, parks = parks)
+
+#CREATE
+@lands_blueprint.route("/lands", methods=['POST'])
+def create_land():
+    name = request.form['name']
+    park_id = request.form['park_id']
+    visited = request.form['visited']
+    park = park_repository.select(park_id)
+    land = Land(name, park, visited)
+    land_repository.save(land)
+    return redirect('/attractions')
